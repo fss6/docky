0f1a625ec9bd9683ebf6e1985e7eec6f569166be
Fábio Santos <fabiosantos.pe@gmail.com>
2026-03-28 17:24:38 -0300

    Add users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_03_28_200532) do
+ActiveRecord::Schema[8.0].define(version: 2026_03_28_201905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -32,5 +32,17 @@ ActiveRecord::Schema[8.0].define(version: 2026_03_28_200532) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.bigint "account_id", null: false
+    t.string "email"
+    t.string "name"
+    t.string "role"
+    t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_users_on_account_id"
+  end
+
   add_foreign_key "accounts", "plans"
+  add_foreign_key "users", "accounts"
 end
---
+++ test/system/users_test.rb
@@ -0,0 +1,51 @@
+require "application_system_test_case"
+
+class UsersTest < ApplicationSystemTestCase
+  setup do
+    @user = users(:one)
+  end
+
+  test "visiting the index" do
+    visit users_url
+    assert_selector "h1", text: "Usuários"
+  end
+
+  test "should create user" do
+    visit users_url
+    click_on "Novo usuário"
+
+    select @user.account_id.to_s, from: "Conta"
+    fill_in "Nome", with: @user.name
+    fill_in "E-mail", with: @user.email
+    fill_in "Função", with: @user.role
+    check "Usuário ativo" if @user.active
+    click_on "Criar usuário"
+
+    assert_text "Usuário criado com sucesso."
+    click_on "Voltar"
+  end
+
+  test "should update User" do
+    visit user_url(@user)
+    click_on "Editar", match: :first
+
+    select @user.account_id.to_s, from: "Conta"
+    fill_in "Nome", with: @user.name
+    fill_in "E-mail", with: @user.email
+    fill_in "Função", with: @user.role
+    check "Usuário ativo" if @user.active
+    click_on "Atualizar usuário"
+
+    assert_text "Usuário atualizado com sucesso."
+    click_on "Voltar"
+  end
+
+  test "should destroy User" do
+    visit user_url(@user)
+    accept_confirm do
+      click_on "Excluir usuário", match: :first
+    end
+
+    assert_text "Usuário excluído com sucesso."
+  end
+end
