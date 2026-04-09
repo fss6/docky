9f81e0441c5156e462d18545edab73662cbc3e1f
Fábio Santos <fabiosantos.pe@gmail.com>
2026-04-09 18:57:01 -0300

    allow_hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,10 @@ Rails.application.configure do
   # Enable server timing.
   config.server_timing = true
 
+  # Know hosts
+  allow_hosts = ENV.fetch("ALLOW_HOSTS") { "" }
+  config.hosts = allow_hosts.split(",")
+
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
