8d8e9642890ba3691af5ed602f85840d1754ac9b
Fábio Santos <fabiosantos.pe@gmail.com>
2026-04-23 14:58:47 -0300

    To dash onChange client

--- app/controllers/current_clients_controller.rb
+++ app/controllers/current_clients_controller.rb
@@ -12,20 +12,11 @@ class CurrentClientsController < ApplicationController
         session[:current_client_id] = client.id
       else
         session.delete(:current_client_id)
-        redirect_back fallback_location: root_path, alert: "Cliente não encontrado.", status: :see_other
+        redirect_to dashboard_path, alert: "Cliente não encontrado.", status: :see_other
         return
       end
     end
 
-    redirect_to redirect_target, status: :see_other
-  end
-
-  private
-
-  def redirect_target
-    return_to = params[:return_to].to_s
-    return return_to if return_to.present? && return_to.start_with?("/")
-
-    request.referer.presence || root_path
+    redirect_to dashboard_path, status: :see_other
   end
 end
--- test/controllers/current_clients_controller_test.rb
+++ test/controllers/current_clients_controller_test.rb
@@ -10,14 +10,14 @@ class CurrentClientsControllerTest < ActionDispatch::IntegrationTest
 
   test "should set current client in session" do
     patch current_client_url, params: { client_id: @client.id }
-    assert_redirected_to root_path
+    assert_redirected_to dashboard_path
     assert_equal @client.id, session[:current_client_id]
   end
 
   test "should clear current client when client_id blank" do
     patch current_client_url, params: { client_id: @client.id }
     patch current_client_url, params: { client_id: "" }
-    assert_redirected_to root_path
+    assert_redirected_to dashboard_path
     assert_nil session[:current_client_id]
   end
 end
