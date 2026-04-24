04f0950c181074cbb3b817265006b151e3353f63
Fábio Santos <fabiosantos.pe@gmail.com>
2026-04-24 14:32:35 -0300

    Set timezone

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Dokivo
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "America/Recife"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_record.yaml_column_permitted_classes = [
       Symbol,
